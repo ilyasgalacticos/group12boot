36609d1470fcea459356690114c5a6640e1cb91d
Ilyas <zhuanyshev.ilyas@gmail.com>
2020-02-27 12:25:45 +0600

    MULTI LANGUAGE

---
+++ src/main/java/kz/iitu/swd/group34/FirstSpringBootIITU/config/WebMVCConfig.java
@@ -0,0 +1,45 @@
+package kz.iitu.swd.group34.FirstSpringBootIITU.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setBasename("classpath:messages");
+        return messageSource;
+    }
+
+    @Bean
+    public CookieLocaleResolver localeResolver(){
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(new Locale("en"));
+        resolver.setCookieName("language-value");
+        resolver.setCookieMaxAge(24*3600*30);
+        return resolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor changeInterceptor(){
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lng");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(changeInterceptor());
+    }
+
+}
